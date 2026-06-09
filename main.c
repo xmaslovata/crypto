f7da11b85830cafcf7340d7a54a48842fda930ca
Arina Maslovata <arinamaslovata@Noutbuk-Arina.local>
2026-06-09 12:47:12 +0300

    Pridana funkcia print_trade

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+
 typedef struct
 {
     char *token;
     char *type;
     double price;
     int time;
-}TRADE;
+} TRADE;
+
+void print_trade(TRADE *trade)
+{
+    printf("Token: %s\n", trade->token);
+    printf("Type: %s\n", trade->type);
+    printf("Price: %lf\n", trade->price);
+    printf("Time: %d\n", trade->time);
+}
+
+int main(void)
+{
+    return 0;
+}
